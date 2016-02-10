07b8285c9460a00fcfde4ad80efdc0c9dd989e19
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2016-02-10 11:09:11 -0600

    Finally got the quit button working

---
+++ Main Menu/Assets/QuitGame.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitGame : MonoBehaviour {
+    
+    public void Quit()
+    {
+        Debug.Log("Im at least being called");
+        // If we are running in a standalone build of the game
+#if UNITY_STANDALONE
+        // Quit the application
+        Application.Quit();
+#endif
+
+        // If we are running in the editor
+#if UNITY_EDITOR
+        // Stop playing the scene
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
