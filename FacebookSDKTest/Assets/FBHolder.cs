009c6c710bbbe4ada70b28ee93f93a513b480bf0
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2016-03-16 23:53:49 -0500

    Started fb loggin tutorial

---
+++ FacebookSDKTest/Assets/FBHolder.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using Facebook.Unity;
+
+public class FBHolder : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // Happens before initialization
+    void Awake ()
+    {
+        FB.Init (SetInit, OnHideUnity);
+    }
+
+    private void SetInit ()
+    {
+        Debug.Log("FB loggin worked");
+
+        if (FB.IsLoggedIn)
+            Debug.Log("Logged in");
+        else
+            Debug.Log("Didint log in");
+    }
+
+
+    private void OnHideUnity (bool isGameShown)
+    {
+        // This is when fb pops in front of the game
+        if (!isGameShown)
+            Time.timeScale = 0;
+    }
+}
