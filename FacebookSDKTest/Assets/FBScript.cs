356a1148b62e01988d18fb3ea25a1442431c31e4
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2016-03-17 21:33:06 -0500

    Created a nice mask to show the profile pic

--- FacebookSDKTest/Assets/FBScript.cs
+++ FacebookSDKTest/Assets/FBScript.cs
@@ -9,6 +9,7 @@ public class FBScript : MonoBehaviour
     public GameObject DialogLoggedIn;
     public GameObject DialogLoggedOut;
     public GameObject WelcomeMessageText;
+    public GameObject UserProfileImage;
 
     // Happens before initialization
     void Awake ()
@@ -69,9 +70,14 @@ public class FBScript : MonoBehaviour
     // Controls UI logged ind and logged out UI
     void SetLoginMenu (bool isLoggedIn)
     {
+        RectTransform user_pic_rectt = UserProfileImage.GetComponent<RectTransform> ();
+        double user_pic_height = user_pic_rectt.rect.height;
+        double user_pic_width = user_pic_rectt.rect.width;
+
         if (isLoggedIn)
         {
             FB.API ("/me?fields=first_name", HttpMethod.GET, DisplayUserName);
+            FB.API ("/me/picture?type=square&height=" + user_pic_height + "&width=" + user_pic_width, HttpMethod.GET, DisplayUserPicture);
         }
 
         DialogLoggedOut.SetActive (!isLoggedIn);
@@ -79,18 +85,37 @@ public class FBScript : MonoBehaviour
     }
 
 
-    // Callback to set username when there's a login
+    // Changes the welcome message to include user name
     void DisplayUserName (IResult result)
     {
-        Text welcome_message = WelcomeMessageText.GetComponent<Text> ();
-
         if (result.Error == null)
         {
+            Text welcome_message = WelcomeMessageText.GetComponent<Text> ();
             welcome_message.text = "How are you, " + result.ResultDictionary ["first_name"] + "?";
         }
         else
         {
             Debug.Log ("Couldn't get user name");
+            Debug.Log (result.Error);
+        }
+    }
+
+    
+    // Displays the user profile picture in a login
+    void DisplayUserPicture (IGraphResult result)
+    {
+        if (result.Error == null)
+        {
+            Image profile_image = UserProfileImage.GetComponent<Image> ();
+            RectTransform user_pic_rectt = UserProfileImage.GetComponent<RectTransform> ();
+            float pic_height = user_pic_rectt.rect.height;
+            float pic_width = user_pic_rectt.rect.width;
+
+            profile_image.sprite = Sprite.Create (result.Texture, new Rect(0, 0, pic_height, pic_width), new Vector2 ());
+        }
+        else
+        {
+            Debug.Log ("Couldn't get user profile picture");
             Debug.Log (result.Error);
         }
     }
